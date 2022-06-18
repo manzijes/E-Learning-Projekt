5f7df99650b48fb4e61f17a0df98cf5d91121796
manzijes <64071693+manzijes@users.noreply.github.com>
2022-06-18 14:08:45 +0200

    effects

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,40 @@
 $(document).ready(function () {
 
+  function fade() {
+    var animation_height = $(window).innerHeight() * 0.25;
+    var ratio = Math.round((1 / animation_height) * 10000) / 10000;
+
+    $('.row div').each(function () {
+
+      var objectTop = $(this).offset().top;
+      var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+
+      if (objectTop < windowBottom) {
+        if (objectTop < windowBottom - animation_height) {
+          $(this).css({
+            transition: 'opacity 0.1s linear',
+            opacity: 1
+          });
+
+        } else {
+          $(this).css({
+            transition: 'opacity 0.25s linear',
+            opacity: (windowBottom - objectTop) * ratio
+          });
+        }
+      } else {
+        $(this).css('opacity', 0);
+      }
+    });
+  }
+  $('.fade').css('opacity', 0);
+  fade();
+  $(window).scroll(function () { fade(); });
+
   // DEVELOPMENT ON // DELETE IF DONE
   $(".disabled").removeClass("disabled");
   $(".confirmation").removeClass("confirmation");
-  
+
   $('.confirmation').on('click', function () {
     return confirm('Bist du sicher, dass du zurück möchtest? Wenn du zurückgehst, geht dein Fortschritt verloren.');
   });
